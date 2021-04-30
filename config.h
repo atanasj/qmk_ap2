89870ab1a602d0aca310a01b4f44fb13e6f9f0fd
atanas <atanas.atanas@gmail.com>
2021-04-30 20:33:58 +1000

    add ap2 keymaps and config

---
+++ config.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
+#define RETRO_TAPPING
+
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 500
+
+#define COMBO_VARIABLE_LEN
+/* #define COMBO_COUNT 13 */
+#define COMBO_TERM 15
+
+#define MK_3_SPEED
+#define MK_MOMENTARY_ACCEL
+
+/* #define IS_COMMAND() (matrix_is_on(3, 0) && matrix_is_on(3, 11)) */
+/* #define IS_COMMAND() (matrix_is_on(0, 1) && matrix_is_on(0, 3) && matrix_is_on(0, 4) && matrix_is_on(0, 5)) */
+/* #define IS_COMMAND() (get_mods() == (MOD_BIT(KC_Q) | MOD_BIT(KC_W) | MOD_BIT(KC_E) | MOD_BIT(KC_R))) */
+
+#define CAPS_LOCATION (MATRIX_COLS * 2 + 11)
+
+/* #define LSPO_KEYS KC_LSFT, KC_LSFT, KC_9 */
+/* #define RSPC_KEYS KC_RSFT, KC_RSFT, KC_0 */
---
+++ rules.mk
@@ -0,0 +1,22 @@
+# Other featues
+CONSOLE_ENABLE = yes
+COMMAND_ENABLE = no
+DEBUG_EEPROM = yes
+LEADER_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+# keep path to combos local to my keymap
+VPATH  +=  keyboards/annepro2/keymaps/atanas/g/
+# SRC    +=  engine.c
+COMBO_ENABLE = yes
+
+ifeq ($(strip $(BOARD)), ANNEPRO2_C18)
+ANNEPRO2_EEPROM = yes
+endif
+
+ifeq ($(strip $(ANNEPRO2_EEPROM)), yes)
+    OPT_DEFS += -DANNEPRO2_EEPROM
+    SRC += spi_master.c eeprom_w25x20cl.c
+endif
+
+MOUSEKEY_ENABLE = yes
